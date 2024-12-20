019200f8e8bd1269e3bb3452ddc3d799e9c6d7ce
Monssaf Toukal <monssaf.toukal@ovhcloud.com>
2024-12-20 15:25:28 -0500

    nmaupu config

---
+++ config.el
@@ -0,0 +1,245 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;;(setq doom-theme 'doom-one)
+(setq doom-font (font-spec :family "CommitMono Nerd Font Mono" :size 17)
+      doom-variable-pitch-font (font-spec :family "CommitMono Nerd Font Mono" :size 17))
+;; (setq doom-theme 'doom-monokai-pro)
+(setq doom-theme 'doom-palenight)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+;;(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/"n
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+ncluding demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Move around windows
+(map! :nivem "C-<left>"  #'windmove-left)
+(map! :nivem "C-<right>" #'windmove-right)
+(map! :nivem "C-<up>"    #'windmove-up)
+(map! :nivem "C-<down>"  #'windmove-down)
+
+;; Resize windows
+(map! :nivem "C-S-<left>"  #'evil-window-decrease-width)
+(map! :nivem "C-S-<right>"             evil-insert-state-map))
+
+(company-terraform-init)
+
+;;(add-hook 'company-mode-hook 'company-box-mode)
+;;(use-package! company-box
+;;  :defer t
+;;  :config
+;;  (setq-hook! 'prog-mode-hook
+;;    company-box-frame-top-margin 20)
+;;  (setq-hook! 'text-mode-hook
+;;    company-box-frame-top-margin 20)
+;;)
+
+;; Save with :w or :W for clumsy fingers
+(evil-ex-define-cmd "W" #'evil-write)
+(evil-ex-define-cmd "X" #'evil-save-and-quit)
+
+
+;;(add-to-list 'company-backends 'companmajor-mode)
+       'puppet-mode)
+       (delete-trailing-whitespace)
+       (doom/delete-trailing-newlines)
+       (puppet-align-block)
+)
+
+(defun before-save-hook-terraform ()
+  (when (eq major-mode 'terraform-mode)
+    (terraform-format-buffer))
+)
+
+(defun before-save-hook-go ()
+  (when (eq major-mode 'reshed notification."
+    (when (equal (gethash "method" notification) "semgrep/rulesRefreshed")
+      (lsp--info "Ignored semgrep/rulesRefreshed notification")
+      t)) ;; Return t to indicate the notification is handled
+  (advice-add 'lsp--on-notification :before-untilterm-mode-hook
+  (unless evil-collection-vterm-send-escape-to-vterm-p
+    (evil-collection-vterm-toggle-send-escape)))
+(map! :after vterm
+      :map vterm-mode-map
+      :ni "C-c" #'vterm--self-insert)
+
+;; popper is better ples!
+  '(
+    (" log\\*" :slot -1)
+   )
+)
+
+;; Disabling buggy treemacs git mode
+(after! treemacs
+  (treemacs-git-mode -1)
+)
+
+;;
+;; loading extra configurations
+(load! "misc/lsp.el")
+(load! "misc/todos.el")
+
+;; TODO Debug
+;; https://discourse.doomemacs.org/t/permanently-display-workspaces-in-the-tab-bar/4088
+;;(load! "misc/ws.el")opups
+(use-package! popper
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$"
+          "\\*Async Shell Command\\*"
+          " log\\*$" ; dap console window
+          help-mode
+          compilation-mode))
+  (popper-mode +1)
+  (popper-echo-mode +1))
+(map! :nivem "C-p"  #'popper-toggle)
+(map! :nivem "M-p"  #'popper-cycle)
+(map! :nivem "C-M-p"  #'popper-toggle-type)
+;; Put the log message buffer at the bottom with a specific size
+(add-to-list 'display-buffer-alist '(" log\\*" display-buffer-at-bottom (window-height . 0.2) (slot -1)))
+(set-popup-ru #'ak-lsp-ignore-semgrep-rulesRefreshed))
+
+;; Display workspace name in modeline
+(after! doom-modeline
+  (setq! doom-modeline-persp-name t))
+
+;; Show indicators in the left fringe
+(after! flycheck
+  (setq! flycheck-indication-mode 'left-fringe))
+
+;;
+;; vterm related configurations
+
+;; Activate clickable url in vterm
+(add-hook! 'vterm-mode-hook
+  (goto-address-mode t))
+
+;; Get C-c and ESC key working
+(add-hook! 'vgo-mode)
+    (gofmt))
+)
+
+;(add-hook! 'before-save-hook #'before-save-hook-puppet)
+(add-hook! 'before-save-hook #'before-save-hook-go)
+(add-hook! 'before-save-hook #'before-save-hook-custom)
+(add-hook! 'before-save-hook #'before-save-hook-terraform)
+
+;; Avoid unwanted semgrep warning
+(after! lsp-mode
+  (defun ak-lsp-ignore-semgrep-rulesRefreshed (workspace notification)
+    "Ignore semgrep/rulesRefy-shell)
+
+;;(add-hook! 'after-init-hook #'global-flycheck-mode)
+
+;;(setq lsp-disabled-clients '(tfls))
+
+;; 'before save a file' hooks
+(defun before-save-hook-custom ()
+  (unless (eql (with-current-buffer (current-buffer) major-mode)
+               'markdown-mode))
+    (delete-trailing-whitespace)
+    (doom/delete-trailing-newlines)
+)
+
+(defun before-save-hook-puppet ()
+  (eql (with-current-buffer (current-buffer) #'evil-window-increase-width)
+(map! :nivem "C-S-<up>"    #'evil-window-increase-height)
+(map! :nivem "C-S-<down>"  #'evil-window-decrease-height)
+
+(map! :nvm "-" 'dired-jump)
+
+;; Navigate through workspaces
+; previous ws
+(map! "M-<left>" #'+workspace/switch-left)
+; next ws
+(map! "M-<right>" #'+workspace/switch-right)
+
+;; (when (display-graphic-p)
+;;   (require 'all-the-icons))
+
+;; Open the shortcut menu quicker
+(setq! which-key-idle-delay 0.3)
+
+;; Selecting text put it in the PRIMARY clipboard
+(setq! evil-visual-update-x-selection-p t)
+
+
+;; Disable mouse entirely
+;; (use-package! disable-mouse)
+;; (global-disable-mouse-mode t)
+;; (mapc #'disable-mouse-in-keymap
+;;       (list evil-motion-state-map
+;;             evil-normal-state-map
+;;             evil-visual-state-map
+;; ;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, i)
+
+(setq show-trailing-whitespace t)
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in a
---
+++ misc/lsp.el
@@ -0,0 +1,147 @@
+;;; go.el -*- lexical-binding: t; -*-
+
+;; Based on:
+;; - https://apmattil.medium.com/debug-go-golang-with-emacs-fbf840c0aa56
+;; - https://github.com/apmattil/.emacs/blob/master/.emacs
+
+;; optional - provides fancy overlay information
+;; https://ladicle.com/post/config/
+(use-package lsp-ui
+  :after(lsp-mode)
+  :commands lsp-ui-mode
+  :config (progn
+            ;; inline documentation
+            (setq lsp-ui-sideline-enable t)
+            ;; showing docs on hover at the top of the window
+            (setq lsp-ui-doc-enable t)
+            (setq lsp-ui-imenu-enable t)
+            (setq lsp-ui-imenu-kind-position 'top)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(lsp-ui-imenu-enable t)
+ '(package-selected-packages
+   '(treemacs dap-mode flycheck-golangci-lint projectile flx-ido yasnippet use-package lsp-ui go-mode flycheck company-lsp))
+ '(tool-bar-mode nil))
+
+(use-package lsp-mode
+  ;; uncomment to enable gopls http debug server
+  ;;:custom (lsp-gopls-server-args '("-debug" "10.236.34.237:30234))
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred)
+  :config (progn
+            ;; use flycheck, not flymake
+            (setq lsp-prefer-flymake nil)
+            ;;(setq lsp-trace nil)
+            (setq lsp-print-performance nil)
+            (setq lsp-log-io nil)))
+
+;; Make sure you don't have other goimports hooks enabled.
+;; (add-hook 'go-mode-hook #'lsp-deferred)
+;; (defun lsp-go-install-save-hooks ()
+;;   (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook                                  (nilness . t)
+                                                (shadow . t)
+                                                (unusedparams . t)
+                                                (unusedwrite . t)
+                                                (useany . t)
+                                                (unusedvariable . box):
+(use-package company-lsp
+  :after(company lsp-mode)
+  :commands company-lsp)
+
+;; Locals vars are now visible and usable
+(after! dap-ui
+  (setq! dap-ui-variable-length 200))
+
+;; Force refreshing treemacs panes
+(lsp-treemacs-sync-mode 1)
+
+;; Make debugger usable using emacs in console
+(unless (display-graphic-p)
+  (set-face-background 'dap-ui-marker-face "color- dap-ui-buffer-configurations
+    `(
+      (,dap-ui--breakpoints-buffer . ((side . left) (slot . 1) (window-width . ,treemacs-width)))
+      (,dap-ui--locals-buffer . ((side . right) (slot . 1) (window-width . 0.3)))
+      (,dap-ui--expressions-buffer . ((side . right) (sssion))
+)
+
+(defun dap-disconnect-custom()
+  (interactive)
+  (dap-disconnect (dap--cur-session))
+  (popper--bury-all)
+)
+
+;; Shortcuts
+(global-set-key [f5]  #'dap-continue)
+(global-set-key [f9]  #'dap-breakpoint-toggle)
+(global-set-key [f10] #'next-expand)
+(global-set-key [f11] #'dap-step-in)
+(global-set-                   "l" #'dap-ui-locals
+        :desc "Eval things at point"      "e" #'dap-eval-thing-at-point
+        :desc "Breakpoint hit condition"  "h" #'dap-breakpoint-hit-condition
+        :desc "Breakpoint condition"      "k" #'dap-breakpoint-condition
+        :desc "Breakpoint log message"    "i" #'dap-breakpoint-log-message
+        :desc "Disconnect"                "Q" #'dap-disconnect-custom
+      )
+)key [f12] #'dap-step-out)
+
+(map! :leader
+      (
+        :prefix ("d" . "DAP debugger menu")
+        :desc "Start a new debug session" "d" #'dap-debug
+        :desc "Next"                      "n" #'next-expand
+        :desc "Continue"                  "c" #'dap-continue
+        :desc "Step In"                   "s" #'dap-step-in
+        :desc "Step Out"                  "o" #'dap-step-out
+        :desc "Go routines"               "g" #'dap-ui-sessions
+        :desc "Locals" lot . 2) (window-width . 0.3)))
+      (,dap-ui--debug-window-buffer . ((side . bottom) (slot . 1) (window-width . 0.20)))
+      (,dap-ui--repl-buffer . ((side . bottom) (slot . 2) (window-height . 0.2)))
+     )
+  )
+)
+
+;; Locals don't expand when doing dap-next, TODO make it work
+;; (defun next-expand()
+;;   (interactive)
+;;   (dap-next (dap--cur-session))
+;;   (sleep-for 1)
+;;   (dap-ui-locals)
+;; )
+(defun next-expand()
+  (interactive)
+  (dap-next (dap--cur-se166") ; An orange background for the line to execute
+  (set-face-attribute 'dap-ui-marker-face nil :inherit nil) ; Do not inherit other styles
+  (set-face-background 'dap-ui-pending-breakpoint-face "blue") ; Blue background for breakpoints line
+  (set-face-attribute 'dap-ui-verified-breakpoint-face nil :inherit 'dap-ui-pending-breakpoint-face)
+)
+
+
+;; Setting windows positions up
+(with-eval-after-load 'dap-ui
+  (treemacs)
+  (setqt)))))
+
+;; Debugger
+(use-package! dap-mode
+  :config
+  (dap-mode 1)
+  (setq! dap-print-io t)
+  (require 'dap-hydra)
+  (require 'dap-dlv-go)
+  (use-package! dap-ui
+    :config
+    (dap-ui-mode 1)
+  )
+  :custom
+  (dap-auto-configure-features '(sessions locals tooltip expressions breakpoints))
+)
+
+;; if you use company-mode for completion (otherwise, complete-at-point works out of the 'go-mode-ho(after! lsp-mode
+                       (setq  lsp-go-analyses '((fieldalignment . t)
+               
