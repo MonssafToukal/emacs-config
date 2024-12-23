452b3874467c18aa3b3be2e2dcd5094717616580
Monssaf Toukal <monssaf.toukal@ovhcloud.com>
2024-12-23 10:51:18 -0500

    add projectile-search path

--- config.el
+++ config.el
@@ -75,6 +75,9 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;
+
+(setq projectile-project-search-path '("~/projects"))
 
 ; Move around windows
 (map! :nivem "C-h"  #'windmove-left)
