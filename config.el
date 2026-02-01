d451697cd0733e93ed4dad6fc3e7c290119cbbb6
Monssaf Toukal <tmonssaf@gmail.com>
2026-01-31 22:58:23 -0500

    put font size to 16

--- config.el
+++ config.el
@@ -21,11 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMonoNL Nerd Font" :size 20)
-      doom-variable-pitch-font (font-spec :family "JetBrainsMonoNL Nerd Font" :size 20))
-
-; (setq doom-font (font-spec :family "JetBrainsMonoNL" :size 20)
-;       doom-variable-pitch-font (font-spec :family "JetBrainsMonoNL" :size 20))
+(setq doom-font (font-spec :family "JetBrainsMonoNL" :size 16)
+      doom-variable-pitch-font (font-spec :family "JetBrainsMonoNL" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
